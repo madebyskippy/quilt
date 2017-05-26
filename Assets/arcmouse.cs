51f288c8016f51810e54707d10388e668ae85a2a
madebyskippy <timothys@nyu.edu>
2017-05-25 22:36:23 -0400

    existing proto

---
+++ Assets/arcmouse.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class arcmouse : MonoBehaviour {
+
+	[SerializeField] arcs manager;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnTriggerEnter2D(Collider2D col){
+		//hit, gameover
+		if (col.gameObject.tag == "grid")
+			manager.hit (true, col);
+		else
+			manager.hit (false, col);
+	}
+}
