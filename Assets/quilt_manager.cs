5c5b26128d5b53e188d8fd72e43bc66166cbb111
madebyskippy <timothys@nyu.edu>
2017-05-27 00:24:06 -0400

    lr draws each poly im defining

---
+++ Assets/quilt_manager.cs
@@ -0,0 +1,263 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class quilt_manager : MonoBehaviour {
+
+	[SerializeField] Vector2 startp;
+
+	[SerializeField] GameObject l;
+	[SerializeField] GameObject circ;
+	[SerializeField] GameObject poly;
+	[SerializeField] Material m;
+
+	[SerializeField] GameObject mouse;
+	[SerializeField] Text text;
+
+	//for the grid
+	Vector2[] grid;
+	Vector2[] stable;
+	Vector2[] changes;
+	int r = 20;
+	int c = 20;
+
+	//for the level
+	List<int[]> quads; //keeps list of groups of index that make up each poly
+	List<GameObject> lines;
+	//for gameplay, specific to level
+	List<GameObject> spaces;
+	List<GameObject> intersections;
+
+	float increment = 0.01f; //change this to speed up the warping
+
+	int count;
+
+	bool gameover;
+	bool win;
+	bool start;
+
+	float score;
+
+	// Use this for initialization
+	void Start () {
+		score = 0;
+		count = 0;
+		text.text = "use the mouse to touch each space\n\n0";
+		gameover = false;
+		win = false;
+		start = true;
+
+		grid 	= new Vector2[r*c];
+		stable 	= new Vector2[r*c];
+		changes = new Vector2[r*c];
+
+		quads = new List<int[]> ();
+		lines = new List<GameObject> ();
+		intersections = new List<GameObject> ();
+		spaces = new List<GameObject> ();
+
+		for (int i = 0; i < r; i++) {
+
+			for (int j = 0; j < c; j++) {
+				grid 	[i*r+j] = new Vector2 (startp.x + i*0.5f, startp.y - j*0.5f);
+				stable	[i*r+j] = new Vector2 (startp.x + i*0.5f, startp.y - j*0.5f);
+				changes [i*r+j] = Vector2.zero;
+				intersections.Add(Instantiate (circ, grid [i*r+j], Quaternion.identity));
+				if (i > 0 && j < c - 1) {
+//					GameObject temp = Instantiate (poly, grid [i * r + j], Quaternion.identity);
+//					temp.GetComponent<arcpoly> ().setP (grid [i * r + j]);
+//					spaces.Add (temp);
+					quads.Add(new int[]{(i-1)*r+j,(i-1)*r+j+1,(i)*r+j+1,(i)*r+j});
+					GameObject t = Instantiate (l, grid [i * r + j], Quaternion.identity);
+					t.GetComponent<LineRenderer> ().SetPositions (new Vector3[]{grid[quads[quads.Count-1][0]],
+						grid[quads[quads.Count-1][1]],
+						grid[quads[quads.Count-1][2]],
+						grid[quads[quads.Count-1][3]],
+						grid[quads[quads.Count-1][0]]
+					});
+					lines.Add (t);
+				}
+			}
+		}
+//		adjustpoly ();
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+		if (Input.GetKeyDown (KeyCode.Space)) {
+			SceneManager.LoadScene ("test");
+		}if (Input.GetKeyDown(KeyCode.Escape)) {
+			Application.Quit();
+		}
+
+		if (Input.GetMouseButtonDown (0)) {
+			if (start) {
+				start = false;
+			}
+			if (gameover || win) {
+				SceneManager.LoadScene ("test");
+			}
+		}
+
+
+
+		Vector3 temp = Camera.main.ScreenToWorldPoint (Input.mousePosition);
+		temp.z = 0;
+
+
+		if (!gameover && !win && !start) {
+
+			warp ();
+
+			score += Time.deltaTime;
+			text.text = "use the mouse to touch each space\n\n" + ((int)score).ToString();
+
+		}
+		if (start) {
+			text.text = "use the mouse to touch each space\n\nclick to start";
+		}
+		if (gameover) {
+			text.text = "don't touch the intersections.\n\nclick to try again";
+		}
+		if (win) {
+			text.text = "amazing. do it again\n\n" + ((int)score).ToString ();
+		}
+		if (gameover || win || start) {
+			jiggle();
+		}
+
+		showlines ();
+		placecirc ();
+		adjustpoly ();
+	}
+
+	void jiggle(){
+		float threshold = 0.05f;
+		for (int j = 0; j < c; j++) {
+			for (int i = 0; i < r; i++) {
+				float x, y;
+				if (grid [i*r+j].x - stable [i*r+j].x > threshold) {
+					x = Random.Range (-1f, 0) * increment;
+				} else if (stable [i*r+j].x - grid [i*r+j].x > threshold) {
+					x = Random.Range (0, 1f) * increment;
+				} else {
+					x = Random.Range (-1f, 1f) * increment;
+				}
+				if (grid [i*r+j].y - stable [i*r+j].y > threshold) {
+					y = Random.Range (-1f, 0) * increment;
+				} else if (stable [i*r+j].y - grid [i*r+j].y > threshold) {
+					y = Random.Range (0, 1f) * increment;
+				} else {
+					y = Random.Range (-1f, 1f) * increment;
+				}
+				changes [i*r+j] = new Vector2 (x, y);
+				edit (i, j, grid [i*r+j] + changes[i*r+j]);
+
+			}
+		}
+	}
+	void warp(){
+		for (int j = 0; j < c; j++) {
+			for (int i = 0; i < r; i++) {
+				//				changes [i, j] = Vector2.one * Random.Range (-1f, 1f) * increment;
+				changes[i*r+j] = new Vector2(Random.Range(-1f,1f),Random.Range(-1f,1f)) * increment;
+				edit (i, j, grid [i*r+j] + changes[i*r+j]);
+
+			}
+		}
+	}
+
+	void edit(int x, int y, Vector2 p){
+		grid [x*r+y] = p;
+	}
+
+	void showlines(){
+		for (int i = 0; i < lines.Count; i++) {
+			lines[i].GetComponent<LineRenderer> ().SetPositions (new Vector3[]{
+				grid[quads[i][0]],
+				grid[quads[i][1]],
+				grid[quads[i][2]],
+				grid[quads[i][3]],
+				grid[quads[i][0]]
+			});
+		}
+	}
+
+	void placecirc(){
+		for (int j = 0; j < c; j++) {
+			for (int i = 0; i < r; i++) {
+				intersections[i*r+j].transform.position=grid[i*r+j];
+			}
+		}
+	}
+
+	void adjustpoly(){
+//		for (int i = 1; i < r; i++) {
+//			for (int j = 0; j < c-1; j++) {
+//				GameObject o = spaces [(i-1)*r+j];
+//				Vector2[] v2;
+//				Vector3[] v3;
+//				v2 = o.GetComponent<PolygonCollider2D> ().points;
+//
+//				v2 [0] = o.transform.InverseTransformPoint(grid [i*r+j]);//-Vector2.one;
+//				v2 [1] = o.transform.InverseTransformPoint(grid [i*r+j+1]	);//-Vector2.one;
+//				v2 [2] = o.transform.InverseTransformPoint(grid [(i-1)*r+j+1]);//-Vector2.one;
+//				v2 [3] = o.transform.InverseTransformPoint(grid [(i-1)*r+j]	);//-Vector2.one;
+//
+//				o.GetComponent<PolygonCollider2D> ().points = v2;
+//				v3 = o.GetComponent<MeshFilter> ().mesh.vertices;
+//				v3 [0] = o.transform.InverseTransformPoint((Vector3)grid [(i-1)*r+j+1]+new Vector3(0,0,1f));//-Vector3.one;
+//				v3 [1] = o.transform.InverseTransformPoint((Vector3)grid [i*r+j]+new Vector3(0,0,1f));//-Vector3.one;
+//				v3 [2] = o.transform.InverseTransformPoint((Vector3)grid [i*r+j+1]+new Vector3(0,0,1f));//-Vector3.one;
+//				v3 [3] = o.transform.InverseTransformPoint((Vector3)grid [(i-1)*r+	j]+new Vector3(0,0,1f));//-Vector3.one;
+//				o.GetComponent<MeshFilter> ().mesh.vertices = v3;
+//				o.GetComponent<MeshFilter> ().mesh.triangles = new int[] {0,1,2,1,0,3};
+//			}
+//		}
+	}
+
+	public void hit(bool corner, Collider2D col = null){
+		if (!gameover && !win){
+			if (corner) {
+				Debug.Log ("hit");
+				col.gameObject.GetComponent<SpriteRenderer> ().enabled = true;
+				col.gameObject.transform.localScale = new Vector3 (0.5f, 0.5f, 0.5f);
+				col.gameObject.transform.GetChild (0).gameObject.SetActive (true);
+				if (!gameover) {
+					Camera.main.backgroundColor = new Color (0.68f,0.68f,0.68f);
+					gameover = true;
+					whitelines ();
+				}
+			} else {
+				if (!col.GetComponent<arcpoly> ().gethit ()) {
+					count++;
+					col.GetComponent<arcpoly> ().hit ();
+					col.GetComponent<MeshRenderer> ().material = m;
+					if (count >= ((r - 1) * (c - 1)) && !win) {
+						win = true;
+						whitelines ();
+						screenshot ();
+					}
+				}
+			}
+		}
+	}
+
+	void whitelines(){
+//		for (int i = 0; i < r; i++) {
+//			lr_r [i].startColor = Color.white;
+//			lr_r [i].endColor = Color.white;
+//			lr_c [i].startColor = Color.white;
+//			lr_c [i].endColor = Color.white;
+//		}
+	}
+
+	void screenshot(){
+		//		string temp = System.DateTime.Now.ToString("yyyyMMddHHmmss");
+		//		Debug.Log (temp);
+		//		Application.CaptureScreenshot("../Screenshot"+temp+".png");
+	}
+}
